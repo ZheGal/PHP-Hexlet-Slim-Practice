78cbe26d26055de10c557e90ffa83aa41e2b505f
Eugeniy Galkovskiy <zhegal@gmail.com>
2020-09-01 17:22:04 +0300

    adding get cources

--- index.php
+++ index.php
@@ -9,13 +9,17 @@ $app = AppFactory::create();
 $app->addErrorMiddleware(true, true, true);
 
 $app->get('/', function ($request, $response) {
-    return $response->getBody()->write('Welcome to Slim!');
+    return $response->write('Welcome to Slim!');
 });
 $app->get('/users', function ($request, $response) {
     return $response->write('GET /users');
 });
 $app->post('/users', function ($request, $response) {
     return $response->withStatus(302);
+});
+$app->get('/courses/{id}', function ($request, $response, array $args) {
+    $id = $args['id'];
+    return $response->write("Course id: {$id}");
 });
 
 $app->run();
