347a373e2f654e7d892fce19133ba1b0ee46cf2e
Eugeniy Galkovskiy <zhegal@gmail.com>
2020-09-01 15:57:35 +0300

    adding get users

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+// Подключение автозагрузки через composer
+require __DIR__ . '/vendor/autoload.php';
+
+use Slim\Factory\AppFactory;
+
+$app = AppFactory::create();
+$app->addErrorMiddleware(true, true, true);
+
+$app->get('/', function ($request, $response) {
+    return $response->getBody()->write('Welcome to Slim!');
+});
+$app->get('/users', function ($request, $response) {
+    return $response->write('GET /users');
+});
+
+$app->run();
