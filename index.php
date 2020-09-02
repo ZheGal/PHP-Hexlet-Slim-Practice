158aed9826863fe63bd30e964bdbca0f3a4f4049
Eugeniy Galkovskiy <zhegal@gmail.com>
2020-09-02 10:27:58 +0300

    search form

--- index.php
+++ index.php
@@ -4,15 +4,41 @@
 require __DIR__ . '/vendor/autoload.php';
 
 use Slim\Factory\AppFactory;
+use DI\Container;
 
-$app = AppFactory::create();
+$users = ['mike', 'mishel', 'adel', 'keks', 'kamila'];
+
+$container = new Container();
+$container->set('renderer', function () {
+    // Параметром передается базовая директория в которой будут храниться шаблоны
+    return new \Slim\Views\PhpRenderer(__DIR__ . '/templates');
+});
+$app = AppFactory::createFromContainer($container);
 $app->addErrorMiddleware(true, true, true);
 
 $app->get('/', function ($request, $response) {
     return $response->write('Welcome to Slim!');
 });
-$app->get('/users', function ($request, $response) {
-    return $response->write('GET /users');
+$app->get('/users', function ($request, $response) use ($users) {
+    $search = $request->getQueryParam('search');
+    $params = ['users' => $users];
+    if ($search) {
+        $find = [];
+        foreach ($users as $user) {
+            if (strpos($user, $search) !== false) {
+                $find[] = $user;
+            }
+        }
+        $params = ['users' => $find];
+    }
+    return $this->get('renderer')->render($response, 'users/index.phtml', $params);
+});
+$app->get('/users/{id}', function ($request, $response, $args) {
+    $params = ['id' => $args['id'], 'nickname' => 'user-' . $args['id']];
+    // Указанный путь считается относительно базовой директории для шаблонов, заданной на этапе конфигурации
+    // $this доступен внутри анонимной функции благодаря https://php.net/manual/ru/closure.bindto.php
+    // $this в Slim это контейнер зависимостей
+    return $this->get('renderer')->render($response, 'users/show.phtml', $params);
 });
 $app->post('/users', function ($request, $response) {
     return $response->withStatus(302);
